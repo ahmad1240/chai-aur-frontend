c6927dfbdfe50757bc0904005a601973202b4593
Mohammad Ahmad <mohammadahmad4@gmail.com>
2023-11-22 18:22:46 +0530

    currency coverter

--- passwordGenrator/src/App.jsx
+++ passwordGenrator/src/App.jsx
@@ -27,6 +27,7 @@ function App() {
     passwordRef.current?.select();
     passwordRef.current?.setSelectionRange(0, 999);
     window.navigator.clipboard.writeText(password);
+    alert("Copied");
   }, [password]);
 
   useEffect(() => {
